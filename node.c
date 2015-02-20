6c6d0fd8a9b58700a53e1c5d0b2b3dc0c120f4b9
g0e9 <g0e9@lcs.ubc.ca>
2015-02-20 14:28:25 -0800

    added test for setting coordinator; added timeout

--- node.c
+++ node.c
@@ -22,6 +22,8 @@ void usage(char * cmd) {
 	 cmd);
 }
 
+//I put all of this in another method so that it would clean up the main function
+//and so that I could reuse it if need be. But mostly because there is a lot in main.
 int init_addrhint(struct addrinfo * hints){
 	memset(hints,0,sizeof(*hints));
 	hints->ai_family=AF_INET;
@@ -53,8 +55,8 @@ int init_group(char * groupListFileName, unsigned int my_port){
 		printf("%d\n", port);
 
 		//Set the addrinfo for each member of the group
-		res = nodes[i];
-		int err=getaddrinfo(hostname,portstr,&hints,&res);
+		//res = nodes[i];
+		int err=getaddrinfo(hostname,portstr,&hints,&nodes[i]);
 		if(err!=0){
 			printf("did not get the addrinfo for %s\n", hostname);
 		}
@@ -78,8 +80,8 @@ int init_group(char * groupListFileName, unsigned int my_port){
 		}
 		i++;
 	}
-	//We may not have 9 members total, so here we take care of the remaining
-	//spots in the vecto_clock.
+	//We may not have 9 members total, so here we initialize the remaining
+	//spots in the vector_clock so that we know they don't exist.
 	total_nodes = i;
 	while (i<MAX_NODES){
 		vector_clock[i].nodeId=0;
@@ -90,7 +92,20 @@ int init_group(char * groupListFileName, unsigned int my_port){
 	return 0;
 }
 
+//we need this method to ensure that the message is in the correct endianness
+//so we go through all the types and convert them
+void net_to_host_msg(struct msg * last_msg){
+	last_msg->msgID = ntohl(last_msg->msgID);
+	last_msg->electionID = ntohl(last_msg->electionID);
+	int i = 0;
+	while(i< MAX_NODES){
+		last_msg->vectorClock[i].nodeId = ntohl(last_msg->vectorClock[i].nodeId);
+		last_msg->vectorClock[i].time = ntohl(last_msg->vectorClock[i].time);
+		i++;
+	}
+}
 
+//This is for debugging purposes; I wanted to make sure the clocks were right
 void print_vector_clock(){
 	int i = 0;
 	printf("{");
@@ -101,12 +116,58 @@ void print_vector_clock(){
 	printf(" N%d:%d }\n", vector_clock[i].nodeId, vector_clock[i].time);
 }
 
+void test_net_to_host_msg(){
+  struct msg tester;
+  tester.msgID = htonl(ELECT);
+  tester.electionID = htonl(5);
+  memcpy(&tester, &vector_clock, sizeof(vector_clock));
+  net_to_host_msg(&tester);
+}
 
+unsigned short get_in_port(struct sockaddr *sa){
+    if (sa->sa_family == AF_INET) {
+        return ntohs(((struct sockaddr_in*)sa)->sin_port);
+    }
+
+    return ntohs(((struct sockaddr_in6*)sa)->sin6_port);
+}
+
+//This function takes the sender and finds that sender in our group.
+//Then we assign the coordinator pointer to that node. Important to remember that our
+//nodes array are pointers to addrinfo structs from which we can get an sockaddr.
+void set_coordinator(struct sockaddr_in * sender){
+	int i = 0;
+	unsigned int node_port= 0;
+	unsigned int sender_port = get_in_port((struct sockaddr *) sender);
+	int done = 0;
+	printf("sender port : %u\n", sender_port);
+	while (i<total_nodes){
+		node_port = get_in_port(nodes[i]->ai_addr);
+		if (node_port == sender_port){
+			//make this guy the coord
+			printf("The coordinator has Id: %d\n", sender_port);
+			coordinator = nodes[i];
+			done = 1;
+		}
+		i++;
+	}
+	if (!done){
+		printf("the person who sent this is not in the group\n");
+	}
+}
+
+//Since I had no way of really testing if things worked I created this
+//method to fake a sender and then set the sender.
+void test_set_coordinator(){
+	struct sockaddr_in sender;
+	sender.sin_family = AF_INET;
+	sender.sin_port = htons(9000);
+	set_coordinator(&sender);
+}
 
 
 int main(int argc, char ** argv) {
 
-  // This is some sample code feel free to delete it
   unsigned long  port;
   char *         groupListFileName;
   char *         logFileName;
@@ -117,10 +178,7 @@ int main(int argc, char ** argv) {
   if (argc != 7) {
     usage(argv[0]);
     return -1;
-}
-  
-
-
+  }
   char * end;
   int err = 0;
 
@@ -189,11 +247,19 @@ int main(int argc, char ** argv) {
 	printf("failed to create socket\n");
 	return -1;
   }
+  struct timeval tv;
+  tv.tv_sec = timeoutValue;
+  tv.tv_usec = 0;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,&tv,sizeof(tv)) < 0) {
+	printf("Could not set timeout on socket\n");
+  }
   if (bind(sockfd, (struct sockaddr *)&si_me, sizeof(si_me))==-1) {
 	close(sockfd);
-	perror("failed to bind");
+	perror("failed to bind\n");
 	return -1;
   }
+  //testing
+  //test_set_coordinator();
 
   //1. We send AYA to the coordinator on every cycle, getting back IAA messages.
   // If we don't get an IAA, then we send an elect to everyone above us.
@@ -203,6 +269,23 @@ int main(int argc, char ** argv) {
   //2c If we don't get any messages back then we send out a coord to everyone below.
   //3. If the message is a coord we just set the coordinator variable to the new node.
   //4. If the message is an elect we send an answer.
+  struct msg last_msg;  //this will always store the last message we receive
+  memset(&last_msg, 0, sizeof(last_msg)); //make sure it doesn't contain any funky values to start
+  struct sockaddr_in sender;  //this is to store who sent the most recent message.
+  memset(&sender, 0, sizeof(sender));
+  //wait to receive a message and timeout if we don't.
+  if(recvfrom(sockfd, &last_msg, sizeof(last_msg), 0,(struct sockaddr *) &sender, sizeof(sender))<0){
+  	printf("timed out on receive\n");
+	//Now we start an election.
+	
+  }
+  else{
+	//make sure the message is in the correct endianness
+	net_to_host_msg(&last_msg);
+	if (last_msg.msgID == COORD){
+		set_coordinator(&sender);
+	}
+  }
 
   close(sockfd);
 
